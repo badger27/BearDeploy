4cd0656dbe73150a8a6fdff226f5ae8c3dec5b61
badger27 <s.freir@gmail.com>
2017-05-29 00:14:13 +0100

    .

---
+++ components/services/services.js
@@ -0,0 +1,29 @@
+
+(function () {
+'use strict';
+
+    angular.module('services.menu', [])
+        
+        .factory('menuService', function() {
+    var showMenu = false;
+
+    return {
+        showMenu: showMenu,
+        state: function() {
+            return showMenu;
+        },
+        toggle: function() {
+            showMenu = !showMenu;
+            return showMenu;
+        },
+        close: function() {
+            showMenu = false;
+            return showMenu;
+        },
+        open: function() {
+            showMenu = true;
+            return showMenu;
+        }
+    };
+});
+}());
