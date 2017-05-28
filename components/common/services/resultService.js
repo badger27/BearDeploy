af3faa14a9542ed7b16eaa2753aa90b3b2ed0a28
badger27 <s.freir@gmail.com>
2017-05-28 22:39:03 +0100

    .

---
+++ components/common/services/resultService.js
@@ -0,0 +1,41 @@
+//This service store the last selected items 
+(function () {
+    'use strict';
+
+    angular.module('services.resultServices', [])
+     .factory('resultServices', [resultServices]);
+   
+    
+
+       function resultServices() {
+           
+           
+         var deleteResults = function(cd){   
+   
+          return 
+         };
+         
+        
+         var  checkResults= function () {
+        
+          return ;
+       
+        };
+        
+          var  setResults= function () {
+        
+          return ;
+       
+        };
+     
+     
+    
+        return {            
+            
+         deleteResults:  deleteResults,
+         checkResults: checkResults
+        };
+        
+        
+    };
+}());
