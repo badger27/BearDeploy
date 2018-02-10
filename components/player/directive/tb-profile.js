78b43b55798402b414702cd7ab1d4fbf5392be68
badger27 <s.freir@gmail.com>
2018-02-10 23:25:46 +0000

    .

---
+++ components/player/directive/tb-profile.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict';
+
+
+    angular.module( 'poolBear.player')
+.directive('tbProfilesCard', function() {
+  return {
+    restrict: 'E',
+    templateUrl: "components/player/directive/templates/profile.html",
+      link:function (scope,element,attr) {
+
+          
+      }
+  };
+})
+
+}());
+
