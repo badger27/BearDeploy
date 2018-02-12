579fa5680d6dcef58d5561caf0571f3482f76015
badger27 <s.freir@gmail.com>
2018-02-12 00:17:28 +0000

    .

---
+++ components/player/directive/tb-statistic.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+
+
+    angular.module( 'poolBear.player')
+.directive('tbStatistic', function() {
+  return {
+    restrict: 'E',
+    templateUrl: "components/player/directive/templates/statistic.html"
+  };
+})
+
+}());
+
