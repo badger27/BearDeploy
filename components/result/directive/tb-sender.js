43681db42132a6e5c398bda126f255c1e36bed0d
badger27 <s.freir@gmail.com>
2018-02-18 01:42:54 +0000

    .

---
+++ components/result/directive/tb-sender.js
@@ -0,0 +1,16 @@
+(function () {
+    'use strict';
+
+
+    angular.module( 'poolBear.result')
+.directive('tbSender', function() {
+  return {
+    restrict: 'E',
+    templateUrl: "components/result/directive/template/sender.html"
+  };
+})
+
+}());
+
+
+
