5cdfda4ae28c765aa0e84cfb6cda79b43be5f1b7
badger27 <s.freir@gmail.com>
2018-04-07 15:12:19 +0100

    .

---
+++ components/result/challenge/challenge-list.js
@@ -0,0 +1,26 @@
+'use strict';
+angular.module('poolBear.result')
+.config(function ($stateProvider) {
+
+    $stateProvider
+      .state('app.challenge', {
+          url: '/player/challenge',
+          data: { title: 'challenge' },
+          views: {
+              'main': {
+                  controller: 'challengeCtrl',
+                  templateUrl: 'components/result/challenge/challenge-list.html'
+
+
+              }
+          }
+
+
+      })
+    
+}).controller('challengeCtrl', ['$scope', function challengeCtrl($scope) {
+    
+    
+    
+}]);
+
