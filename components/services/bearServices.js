531a8d4c499cfab41f6b83ec8e75860b09db7cd7
badger27 <s.freir@gmail.com>
2018-04-08 00:08:40 +0100

    .

---
+++ components/services/bearServices.js
@@ -0,0 +1,29 @@
+
+(function () {
+'use strict';
+
+   angular.module('services.bears', [])
+     .factory('selector', ['$http', function() {
+
+
+    
+    //SET Layout 
+    var  setLayout = function()  {
+                        
+          };
+          
+     //SET Layout 
+    var setBearclass= function () {
+                        
+          };
+
+    return  {
+        setLayout:setLayout,
+        setBearclass:setBearclass 
+    };
+
+     }])
+     }());
+      
+       
+
