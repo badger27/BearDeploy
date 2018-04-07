622649eec6874ab12842d73f65d1d080c11793da
badger27 <s.freir@gmail.com>
2018-04-07 13:33:57 +0100

    .

---
+++ components/common/directives/_appPopMenue.js
@@ -0,0 +1,40 @@
+angular.module("poolBear.PopMenu", [])
+
+        .controller('PopMenuCtrl', function ($scope){
+            
+            console.log("messageController", $scope);
+    
+    
+           $scope.messageInDev = {
+               
+               note:"developer"
+           }
+           
+             $scope.messagePremium = {
+               
+               note:"premium"
+           }
+            
+
+        })
+
+        .directive('notavailable',  function () {
+          return{
+             templateUrl: './components/directives/view/indevMessage.html',
+              restrict: 'E',
+              scope:{
+                  name: '=',
+                  name2: '='
+              },
+               
+              controller: function($scope){
+        
+                  $scope.indevelopment =function( messageInDev){
+                    
+                   $scope.collapsed= ! $scope.collapsed;
+                   $scope.collapsedp= ! $scope.collapsedp;
+                  }
+                   console.log("messageDirective", $scope);
+              }
+          }  
+        }); 
