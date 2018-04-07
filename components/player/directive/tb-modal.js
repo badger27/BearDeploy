ff4fca63bbdcce2f870f2f6d601ba42b7050679f
badger27 <s.freir@gmail.com>
2018-04-07 13:29:29 +0100

    .

---
+++ components/player/directive/tb-modal.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+
+
+    angular.module( 'poolBear.player')
+.directive('tbModal', function() {
+  return {
+    restrict: 'E',
+    templateUrl: "components/player/directive/templates/modal.html"
+  };
+})
+
+}());
+
