4463611429b8211afdc15622d79cffcc6746435c
badger27 <s.freir@gmail.com>
2018-04-24 23:58:45 +0100

    .

---
+++ components/common/constants/_theme.js
@@ -0,0 +1,9 @@
+angular.module("themeConfig", [])
+.constant("pool", "_poolBear")
+.constant("languageID", " 221")
+.constant("languageCode", "en")
+.constant("deployPath", "/Volumes/Macintosh HD 2/dev/microsoft-staging/BearDeploy/")
+.constant("deployDist", "/Volumes/Macintosh HD 2/dev/The-Bear/dist/")
+.constant("themeData", ["'poolBear'","'footballBear'","'SquashBear'","'golfBear'","'cSBear'","'tennisBear'"])
+.constant("ratingSystem", {"standard":"_standard","advanced":"_advanced","custom":"_custom"})
+.constant("app", ["'firebase'","'ui.router'","'poolBear.login'","'poolBear.auth'","'poolBear.result'","'poolBear.settings'","'poolBear.actionmodal'","'poolBear.scoreMessage'","'poolBear.Message'","'poolBear.uploadFile'","'poolBear.Images'","'poolBear.Images'","'poolBear.uploadFileService'","'poolBear.search'","'poolBear.challenge'","'poolBear.group'","'poolBear.geo'","'poolBear.bio'","'poolBear.chat'","'poolBear.shop'","'poolBear.api'","'poolbear.confirmServices'","'services.menu'","'slickCarousel'","'slickExampleApp'"]);
