06628a00f59d2d247eebf2132a432eb2e2df41cd
sfreir <sfreir@shutterstock.com>
2019-10-31 09:23:25 +0000

    deploy

---
+++ components/bio/bio.js
@@ -0,0 +1,43 @@
+(function(){
+"use strict";
+
+
+
+angular.module('poolBear.bio', [
+
+
+])
+    .config(function ($stateProvider) {
+
+        $stateProvider
+            .state('app.bio', {
+                url: '/bio',
+                data: {title: 'bio'},
+                views: {
+                    'main': {
+                        controller: 'bioController',
+                        templateUrl: 'components/bio/bio.html',
+
+                        //This code restricts access to logged in users only
+                        resolve: {
+                            currentAuth: function (Authentication) {
+                                return Authentication.requireAuth();
+                            } //current Auth
+                        } //resolve
+                    }
+                }
+            })
+        })
+
+
+    .controller('bioController',
+        ['$scope', '$rootScope', '$firebaseAuth', '$firebaseArray', '$firebaseObject', 'FIREBASE_URL', 'firebaseDataService',
+            function ($scope, $rootScope, $firebaseAuth, $firebaseArray, $firebaseObject, FIREBASE_URL, firebaseDataService) {
+
+            console.log("BIO FIRED" );
+
+
+
+            }])
+
+}());
