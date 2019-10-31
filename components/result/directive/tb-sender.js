ef24f03cd499deccf6a47783b2dab43ff7804b7d
sfreir <sfreir@shutterstock.com>
2019-10-31 10:57:22 +0000

    test

---
+++ components/result/directive/tb-sender.js
@@ -0,0 +1,15 @@
+
+    'use strict';
+
+    angular.module( 'poolBear.result')
+.directive('tbSender', function() {
+  return {
+    restrict: 'E',
+    templateUrl: "components/result/directive/template/sender.html"
+  };
+})
+
+
+
+
+
---
+++ components/services/theme-service.js
@@ -0,0 +1,23 @@
+
+    'use strict';
+
+    angular.module('poolBear')
+
+        .factory('themeService', function () {
+            let theme = "";
+            let setTheme = function (themeName) {
+                console.log("SET THEME FIRED ");
+                theme = themeName
+            }
+
+            let getTheme = function (callback) {
+                console.log("GET THEME FIRED");
+                callback(theme);
+            }
+
+            return {
+                getTheme: getTheme,
+                setTheme: setTheme,
+
+            };
+        });
