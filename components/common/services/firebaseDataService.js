da4624497ac1156878dc2ef7b3b52f9bdab86196
sfreir <sfreir@shutterstock.com>
2019-10-31 08:14:42 +0000

    deploy

---
+++ components/common/services/firebaseDataService.js
@@ -0,0 +1,114 @@
+// FIREBASE DATA SERVICE START
+(function () {
+    'use strict';
+    angular.module('poolBear.player')
+        .factory('firebaseDataService', ['$rootScope', '$firebaseAuth', '$firebaseArray', '$firebaseObject', 'FIREBASE_URL',
+            function ($rootScope, $firebaseAuth, $firebaseArray, $firebaseObject, FIREBASE_URL) {
+
+            var auth = firebase.auth();
+                var database = firebase.database();
+
+            let theme = $rootScope.theme;
+            if( $rootScope.theme === undefined || null){
+                // theme=  window.localStorage.getItem("theme");
+                theme="poolbear";
+                theme= theme.toLowerCase();
+            }else{
+                console.log("DATASERVICEs THEME" );
+                theme= theme.toLowerCase();
+            }
+
+
+
+                //GET-CURRENT-USER-RESULTS START
+                let getCurrentUserResults = function (callback) {
+
+                    auth.onAuthStateChanged(function (authUser) {
+                        if (authUser) {
+                            let resultsArr = [];
+                            var userId = firebase.auth().currentUser.uid;
+                            var individualRecordUserRef = database.ref( 'bears/' + theme + '/result/' + userId );
+
+                            individualRecordUserRef.once('value', function (snapshot) {
+
+                                snapshot.forEach(childSnapshot => {
+                                    let item = childSnapshot.val();
+                                    item.key = childSnapshot.key;
+                                    resultsArr.push(item);
+                                });
+
+                                callback(resultsArr);
+                            });
+
+                        } // User Authenticated
+                    }); // on Auth
+                };
+                //GET-CURRENT-USER-RESULTS START
+
+
+                //GET-PLAYERS
+                var getPlayers = function (callback) {
+                    auth.onAuthStateChanged(function (authUser) {
+                        if (authUser) {
+                            let returnArr = [];
+                            var PlayerUserRefAll = database.ref('bears/' + theme + '/users');
+                            PlayerUserRefAll.once('value', function (snapshot) {
+
+                                snapshot.forEach(childSnapshot => {
+                                    let item = childSnapshot.val();
+                                    item.key = childSnapshot.key;
+                                    returnArr.push(item);
+                                });
+
+                                callback(returnArr);
+                            });
+                        } // User Authenticated
+                    }); // on Auth
+                };
+                //GET-PLAYERS END
+
+
+
+                //GET-PLAYERS
+                var getNotifications = function (callback) {
+                    auth.onAuthStateChanged(function (authUser) {
+                        if (authUser) {
+                            let returnArr = [];
+
+                            let userId = firebase.auth().currentUser.uid;
+                            let notificationRef = database.ref('bears/poolbear/notification/' + userId);
+
+                            notificationRef.once('value', function (snapshot) {
+
+                                _.toArray(snapshot.val())
+                                // snapshot.forEach(childSnapshot => {
+                                //     let item = childSnapshot.val();
+                                //     item.key = childSnapshot.key;
+                                //     returnArr.push(item);
+                                // });
+
+                                     var notificationsList = _.toArray(snapshot.val())
+
+                                callback(notificationsList);
+                            });
+                        } // User Authenticated
+                    }); // on Auth
+                };
+                //GET-PLAYERS END
+
+
+
+
+                return {
+
+                    getCurrentUserResults: getCurrentUserResults,
+                    getPlayers:getPlayers,
+                    getNotifications:getNotifications
+
+
+                };
+
+            }
+        ]);
+}());
+
---
+++ components/player/directive/tb-statistic.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+
+
+    angular.module( 'poolBear')
+.directive('tbStatistic', function() {
+  return {
+    restrict: 'E',
+
+    templateUrl: "components/player/directive/templates/statistic.html"
+  };
+})
+
+}());
+
