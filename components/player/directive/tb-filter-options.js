8782b337da16c2907dc860f8ad78304b671a28f4
sfreir <sfreir@shutterstock.com>
2019-10-31 09:35:35 +0000

    deploy

---
+++ components/player/directive/tb-filter-options.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+    angular.module( 'poolBear')
+.directive('tbFilterOptions', function() {
+  return {
+    restrict: 'E',
+    templateUrl: "components/player/directive/templates/tb-filter-options.html"
+  };
+})
+
+}());
+
---
+++ components/services/bearServices.js
@@ -0,0 +1,26 @@
+(function () {
+    'use strict';
+
+    angular.module('services.bears', [])
+        .factory('selector', ['$http', function () {
+
+            //SET Layout
+            var setLayout = function () {
+
+            };
+
+            //SET Layout
+            var setBearclass = function () {
+
+            };
+
+            return {
+                setLayout: setLayout,
+                setBearclass: setBearclass
+            };
+
+        }])
+}());
+      
+       
+
