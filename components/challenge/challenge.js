a3a38125d03ccc280c3104829e63160330d563f7
sfreir <sfreir@shutterstock.com>
2019-11-16 16:09:56 +0000

    test

---
+++ components/challenge/challenge.js
@@ -0,0 +1,32 @@
+'use strict';
+angular.module('poolBear.challenge', [
+    
+   
+])
+.config(function ($stateProvider) {
+
+
+    $stateProvider
+      .state('app.challenge', {
+           
+          
+          url: '/challenge',
+          data: { title: 'challenge' },
+          views: {
+              'main': {
+                  controller: 'challengeCtrl',
+                  templateUrl: 'components/challenge/challenge.html'
+
+
+              }
+          }
+
+
+      })
+    
+}).controller('challengeCtrl', ['$scope', function challengeCtrl($scope) {
+    
+    
+    
+}]);
+
---
+++ components/common/constants/_theme.js
@@ -0,0 +1,11 @@
+angular.module("themeConfig", [])
+.constant("pool", "_poolBear")
+.constant("playerFilter", [{"filterId":1,"filterName":"PLATFORM"},{"filterId":2,"filterName":"PLAYED"}])
+.constant("sortFilter", [{"sortId":1,"filterName":"elo"},{"sortId":2,"filterName":"wins"},{"sortId":3,"filterName":"lost"}])
+.constant("languageID", " 221")
+.constant("languageCode", "en")
+.constant("deployPath", "/Users/sfreir/projects/Stephen/bear-deploy/")
+.constant("deployDist", "/Users/sfreir/projects/Stephen/The-Bear/dist/")
+.constant("themeData", ["'poolBear'","'footballBear'","'SquashBear'","'golfBear'","'cSBear'","'tennisBear'"])
+.constant("ratingSystem", {"standard":"_standard","advanced":"_advanced","custom":"_custom"})
+.constant("app", ["'firebase'","'ui.router'","'poolBear.login'","'poolBear.auth'","'poolBear.result'","'poolBear.settings'","'poolBear.actionmodal'","'poolBear.scoreMessage'","'poolBear.Message'","'poolBear.uploadFile'","'poolBear.Images'","'poolBear.uploadFileService'","'poolBear.search'","'poolBear.challenge'","'poolBear.group'","'poolBear.geo'","'poolBear.bio'","'poolBear.chat'","'poolBear.api'","'poolbear.confirmServices'","'themeConfig'","'services.menu'","'services.bears'","'slickCarousel'","'slickExampleApp'"]);
